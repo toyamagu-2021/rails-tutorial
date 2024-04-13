5d7a6a4df43ce18dd71fa04801b43f5b2080d1c7
toyamagu2021 <toyamagu2021@gmail.com>
2024-04-14 00:43:11 +0900

    feat: chapter 7
    Signed-off-by: toyamagu2021 <toyamagu2021@gmail.com>

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,27 @@
+require "test_helper"
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "valid signup new user" do
+    get signup_path
+    assert_difference 'User.count' do
+      post users_path, params: { user: { name:  "test",
+                                        email: "test@example.com",
+                                        password:              "testtest",
+                                        password_confirmation: "testtest" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
+  test "invalid signup new user" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name:  "",
+                                        email: "user@invalid",
+                                        password:              "foo",
+                                        password_confirmation: "bar" } }
+    end
+    assert_response :unprocessable_entity
+    assert_template 'users/new'
+  end
+end
