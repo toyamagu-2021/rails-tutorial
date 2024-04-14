4f51d9fcad9cf488638209695b565b67148bdbcf
gussan <83329336+toyamagu-2021@users.noreply.github.com>
2024-04-14 13:55:49 +0900

    feat: chapter 8 (#7)
    Signed-off-by: toyamagu2021 <toyamagu2021@gmail.com>

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -5,4 +5,5 @@ class ApplicationHelperTest < ActionView::TestCase
     assert_equal "Ruby on Rails Tutorial Sample App", full_title
     assert_equal "Help | Ruby on Rails Tutorial Sample App", full_title("Help")
   end
+
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -12,6 +12,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_template 'users/show'
     assert_not flash.empty?
+    assert is_logged_in?
   end
   test "invalid signup new user" do
     get signup_path
