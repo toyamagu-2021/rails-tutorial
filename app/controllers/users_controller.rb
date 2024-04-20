d628e7cd0414a7094f712166d6e1df0e0c4f7ac4
toyamagu-2021 <toyamagu2021@gmail.com>
2024-04-21 00:15:09 +0900

    feat: chap 11
    Signed-off-by: toyamagu-2021 <toyamagu2021@gmail.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,16 @@ class UsersController < ApplicationController
   def index
     @users = User.paginate(page: params[:page])
   end
+
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
+
   def new
     @user = User.new
   end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -42,23 +46,17 @@ class UsersController < ApplicationController
   end
 
   private
+
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
         # beforeフィルタ
 
-    # ログイン済みユーザーかどうか確認
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url, status: :see_other
-      end
-    end
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url, status: :see_other) unless current_user?(@user)
     end
+
     # 管理者かどうか確認
     def admin_user
       redirect_to(root_url, status: :see_other) unless current_user.admin?
